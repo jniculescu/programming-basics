20a0286654fcb3f6cad39949255cf8fb4c0f2978
jniculescu <jani.niculescu@gmail.com>
2017-09-21 15:43:47 +0300

    Task 1 trololo

---
+++ conditional-statements/conditional-statements/conditional-statements/Program.cs
@@ -0,0 +1,43 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditional_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma antaa joko pos, neg tai nolla");
+            Console.Write("Syötä numero: ");
+
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int evaluatedNumber;
+            int.TryParse(userInput, out evaluatedNumber);
+
+            Console.WriteLine("Syötit arvon {0}", userInput);
+            Console.WriteLine($"Syötit arvon: {userInput}");
+
+            if(evaluatedNumber < 0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on negatiivinen");
+            }
+
+            else if (evaluatedNumber > 0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on positiivinen");
+            }
+
+            else if (evaluatedNumber == 0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on nolla");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
